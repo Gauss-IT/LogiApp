dd46c01b2b78dc697728f65a3d885ca04904b93d
Abdi Beqiri <abdi.beqiri@hotmail.com>
2019-09-22 20:24:48 +0200

    First implementation of login,register and showdata forms

---
+++ LoginApp/EntryForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class EntryForm : Form
+    {
+        public EntryForm()
+        {
+            InitializeComponent();
+            var dbContext = new Users();
+            var binding = new BindingSource { DataSource = dbContext.Useres.ToList() };
+            dtgUsers.DataSource = binding;
+        }
+
+
+    }
+}
--- LoginApp/RegisterUsers.cs
+++ LoginApp/RegisterUsers.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LoginApp
 {
-    public partial class Form1 : Form
+    public partial class RegisterUsers : Form
     {
-        public Form1()
+        public RegisterUsers()
         {
             InitializeComponent();
         }
